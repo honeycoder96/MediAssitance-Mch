dd0ae0d2d84cdfb8704f3a88377ebad013efe502
kaus2595 <kausr2595@gmail.com>
2018-02-04 02:55:00 +0530

    Minor changes

--- UI/index.js
+++ UI/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-// import AgentEnquiries from './AgentEnquiries/AgentEnquiries.js';
 import registerServiceWorker from './registerServiceWorker';
 import homepage from './index.jade';
 
